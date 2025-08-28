64a2f744268473ad9d04d811c44b9febd708fd16
Edward Ellis <edwardellis.egre@gmail.com>
2025-08-28 02:55:12 +0100

    First commit, projects build independently

---
+++ inventory-springboot/src/test/java/net/ellise/inventory/springboot/InventorySpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ellise.inventory.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventorySpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ users-springboot/src/main/java/net/ellise/users/springboot/UsersSpringbootApplication.java
@@ -0,0 +1,13 @@
+package net.ellise.users.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersSpringbootApplication.class, args);
+	}
+
+}
