2b3189b58debfa1e7969f118a2f394d9416221c7
Edward Ellis <edwardellis.egre@gmail.com>
2025-09-02 20:47:09 +0100

    Dynamic table uses variables

--- frontend-react/src/DynamicTable.js
+++ frontend-react/src/DynamicTable.js
@@ -1,24 +1,21 @@
 import React from 'react';
 
 function DynamicTable({headers, validations, details}) {
+    var tableHeader = headers.map((header, index) => React.createElement("th", {}, header.header));
+    var tableData = details.map((detail, offset) =>
+                                        React.createElement("tr", {},
+                                            headers.map((header, index) =>
+                                                React.createElement("td", {}, detail[header.id])
+                                            )
+                                        )
+                                    )
+
     return (
         <table>
             <tr>
-                {
-                    headers.map((header, index) =>
-                        React.createElement("th", {}, header.header)
-                    )
-                }
+                {tableHeader}
             </tr>
-            {
-                details.map((detail, offset) =>
-                    React.createElement("tr", {},
-                        headers.map((header, index) =>
-                            React.createElement("td", {}, detail[header.id])
-                        )
-                    )
-                )
-            }
+            {tableData}
         </table>
     );
 }
